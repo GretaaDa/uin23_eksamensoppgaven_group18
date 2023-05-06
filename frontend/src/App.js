fe8fc79edae7b161adc990be162220fd453cb112
pvdec <diogo@decifrar.net>
2023-05-06 21:05:23 +0200

    added active class

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,19 @@
 import './App.scss';
-import Dashboard from './components/Dashboard';
+import Nav from './components/Nav';
 import MyGames from './components/MyGames';
 import MyFavourites from './components/MyFavourites';
 import Gameshop from './components/GameShop';
 import { Route, Routes } from "react-router-dom"
+import Dashboard from './components/Dashboard';
 
 
 function App() {
   return (
     <>
-    <Dashboard/>
+    <Nav/>
     <div className="container">
       <Routes>
+        <Route path="Dashboard" element={<Dashboard/>} />
         <Route path="GameShop" element={<Gameshop/>} />
         <Route path="MyGames" element={<MyGames/>} />
         <Route path="MyFavourites" element={<MyFavourites/>} />
--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,17 +1,5 @@
-export default function Dashboard(){
-    return <nav className="nav">
-        <a href="/" className="title">MACs Gamehub</a>
-        <ul>
-            <li>
-                <a href="/Gameshop">Shop</a>
-            </li>
-            <li>
-                <a href="/MyGames">My Games</a>
-            </li>
-            <li>
-                <a href="/MyFavourites">Favourites</a>
-            </li>
-        </ul>
-
-    </nav>
+export default function Dashboard() {
+    return(
+    <h2>Dashboard</h2>
+    )
 }
