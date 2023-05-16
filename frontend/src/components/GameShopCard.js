3d023001f13e8536715b0091a2e828a5f58dd689
pvdec <diogo@decifrar.net>
2023-05-16 23:23:14 +0200

    scss

--- frontend/src/components/GameShopCard.js
+++ frontend/src/components/GameShopCard.js
@@ -1,17 +1,15 @@
 import { useEffect, useState } from 'react';
+import AddFavourite from './AddFavourite';
+
 
 export default function GameShopCard({recent}) {
     return (<>
         <div className="gameshopCard">
             <img className="thumbnail" src={recent.background_image} alt = {recent.name}/>
             <h3>{recent.name}</h3>
-            {recent?.genres.map((genres, i) => <p key={i}>{genres.name}</p>)}
-            <button>Buy</button>
+            {recent?.genres.map((genres, i) => <p className='genres' key={i}>{genres.name}</p>)}
+            <button className='favBtn'><AddFavourite/></button>
+            <button className='buyBtn'>Buy</button>
         </div>
     </>)
 }
-
-//{recent?.genres?.map((recent, i) => <GameShopCard key={i} recent={recent}/>)}
-//<button>Buy{recent.stores.map}</button>
-
-//https://api.rawg.io/api/platforms
