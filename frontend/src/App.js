ab7924d4783ec92ba823e0a89208b31496468ba1
pvdec <diogo@decifrar.net>
2023-05-16 19:50:50 +0200

    store api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,24 +9,44 @@ import { useEffect, useState } from 'react';
 import { fetchGames } from './sanity/gameServices';
 
 //7defe92ceef1441682a9b46c202cbc3d
+//https://api.rawg.io/api/games?dates=2019-09-01,2019-09-30&platforms=18,1,7&key=5e35f504c4154714add5b9909f65f051
 function App() {
   const [games, setGames] = useState(null)
 
-  const [recentGames, setRecentGames] = useState([])
-
   const getRecentGames = async () => {
-    const result = await fetch(`https://api.rawg.io/api/games?dates=2019-09-01,2019-09-30&platforms=18,1,7&key=5e35f504c4154714add5b9909f65f051`)
-    const data = await result.json()
-    setRecentGames(data)
+    const data = await fetch(
+      `https://api.rawg.io/api/games?key=5e35f504c4154714add5b9909f65f051&platforms=18,1,7`
+    ).then((res) => res.json());
+
+    const games2 = data.results;
+
+    const updatedGames = await Promise.all(
+        games2.map(async (game) => {
+            const data = await fetch(
+                `https://api.rawg.io/api/games/${game.slug}/stores?key=5e35f504c4154714add5b9909f65f051`
+            ).then((res) => res.json());
+
+            const stores = data.results;
+            const storeUrl = stores.find((store) => store.url.includes('steam')).url;
+
+            return {
+                ...game,
+                store_url: storeUrl,
+            };
+        })
+    );
+
+    setGames(updatedGames);
+};
+  const getGames = async () => {
+    const data = fetchGames()
   }
+
+console.log(setGames)
 
   useEffect(() => {
     getRecentGames()
   }, [])
-
-  const getGames = async () => {
-    const data = fetchGames()
-  }
 
   useEffect(() => {
     getGames()
@@ -36,8 +56,8 @@ function App() {
       <Nav />
       <div className="container">
         <Routes>
-          <Route index element={<Dashboard recentGames={recentGames}/>} />
-          <Route path="GameShop" element={<Gameshop recentGames={recentGames}/>} />
+          <Route index element={<Dashboard games={games}/>} />
+          <Route path="GameShop" element={<Gameshop games={games}/>} />
           <Route path="MyGames" element={<MyGames />} />
           <Route path="MyFavourites" element={<MyFavourites />} />
         </Routes>
--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,10 +1,10 @@
 import GameShopCard from "../components/GameShopCard"
 
-export default function Dashboard({recentGames}) {
-    recentGames = recentGames?.results?.slice(0, 3)
+export default function Dashboard({games}) {
+    games = games?.slice(0, 3)
     return(<>
     <h2>Dashboard</h2>
-    {recentGames?.map((recent, i) => <GameShopCard key={i} recent={recent}/>)}
+    {games?.map((recent, i) => <GameShopCard key={i} recent={recent}/>)}
     
     </>)
 }
