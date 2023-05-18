b5144c770b991a0084564c6f517373967ff012b5
pvdec <diogo@decifrar.net>
2023-05-18 15:43:51 +0200

    updated scss

--- frontend/src/components/GameShopCard.js
+++ frontend/src/components/GameShopCard.js
@@ -1,11 +1,14 @@
 import { useEffect, useState } from 'react';
+import AddFavourite from './AddFavourite';
 
 export default function GameShopCard({ recent }) {
+    const genresToShow = recent.genres.slice(0, 2); // Get only the first 2 genres, rather than every one of them
+
     /*Prepared the useState hook with two values, recentGameId used to display the current state and setRecentGameId function
     to update the state*/
     const [recentGameId, setRecentGameId] = useState([])
 
-    //Fentched API info for the game stores to get a new array (results) with the store information
+    //Fetched API info for the game stores to get a new array (results) with the store information
     const getRecentGameId = async () => {
         const result = await fetch(`https://api.rawg.io/api/games/${recent.id}/stores?&key=5e35f504c4154714add5b9909f65f051`)
         const data = await result.json()
@@ -22,9 +25,12 @@ export default function GameShopCard({ recent }) {
     return (<>
         <div className="gameshopCard">
             <img className="thumbnail" src={recent.background_image} alt={recent.name} />
-            <h3>{recent.name}</h3>
-            {recent?.genres.map((genres, i) => <p key={i}>{genres.name}</p>)}
-            <button>Buy</button>
+            <h3 className='gameName'>{recent.name}</h3>
+            {genresToShow.map((genre, i) => (
+          <p key={i}>{genre.name}</p>
+        ))}
+            <button className='buyBtn'>Buy</button>
+            <button><AddFavourite></AddFavourite></button>
         </div>
     </>)
 }
--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,12 +1,18 @@
 import GameShopCard from "../components/GameShopCard"
 import GameCard from "../components/GameCard";
+import {Link} from "react-router-dom"
 
 export default function Dashboard({ recentGames, games }) {
     recentGames = recentGames?.results?.slice(0, 3)
     games = games.slice(0, 4)
     return (<>
         <h2>Dashboard</h2>
+            <Link to="/GameShop">
+                <button className="visit">Visit Shop</button>
+            </Link>
+        <div className="newest">
         {recentGames?.map((recent, i) => <GameShopCard key={i} recent={recent} />)}
+        </div>
         {games?.map((game, i) => <GameCard key={i} game={game} />)}
 
     </>)
